43853ab27036e98d8fd44650cb58be7c9e0ceb80
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-05 17:22:25 -0400

    update

--- front/src/components/Category.js
+++ front/src/components/Category.js
@@ -19,7 +19,7 @@ const Category = () => {
 <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card">
-      <img src=".https://images.pexels.com/photos/2506988/pexels-photo-2506988.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" class="card-img-top" alt="..." />
+      <img src="https://images.pexels.com/photos/2506988/pexels-photo-2506988.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" class="card-img-top" alt="..." />
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
