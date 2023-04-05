609e747893effa28a7a7e0d84b364f0a0fc5ad23
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-05 16:01:40 -0400

    update

--- front/src/components/Featured.js
+++ front/src/components/Featured.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <section>
+        <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://images.pexels.com/photos/274249/pexels-photo-274249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src="https://images.pexels.com/photos/274249/pexels-photo-274249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src="https://images.pexels.com/photos/274249/pexels-photo-274249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="..." />
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </section>
   )
 }
 
