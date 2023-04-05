96985077ffc618906efe6e7b3e6ba5876b4ff19f
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-05 14:57:14 -0400

    update

--- back/app.js
+++ back/app.js
@@ -1,13 +1,16 @@
 
 
-const express = require("express");
-const app = express();
-
-
-const mongoose = require('mongoose');
+const express = require('express')
+const mongoose = require("mongoose")
+const dotenv = require('dotenv').config()
+const cors = require('cors')
 const port = process.env.PORT || 5001;
-
+const authController = require("./controller/authController.js")
+const app = express()
+const blogController = require("./controller/blogController")
 require('dotenv').config();
+
+
 
 
 const uri = process.env.ATLAS_URI;
@@ -16,10 +19,18 @@ mongoose.connect(uri, { useNewUrlParser: true }
 );
 const connection = mongoose.connection;
 
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/auth', authController)
+app.use("/blog", blogController)
+
+
+
 connection.once('open', () => {
   console.log("MongoDB Connected");
 })
 
 app.listen(port, () => {
-    console.log("PORT LISTENING")
+    console.log(`${port} going`)
 })
