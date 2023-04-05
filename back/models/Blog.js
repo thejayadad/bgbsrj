6999aef4dd48bd5aee32a9fcfd32c9ac507bb667
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-05 15:37:46 -0400

    update

--- back/models/Blog.js
+++ back/models/Blog.js
@@ -24,6 +24,10 @@ const BlogSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    featured: {
+        type: Boolean,
+        default: false,
+    },
     views: {
         type: Number,
         default: 0
